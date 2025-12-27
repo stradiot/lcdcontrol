51699243fe5faae5e4d8e0a21c059243101d305e
Martin Stradiot <martin.stradiot@gmail.com>
2025-12-27 14:26:20 +0000

    fix: correct pin numbers

--- include/hd44780.h
+++ include/hd44780.h
@@ -3,12 +3,15 @@
 
 #define GPIO_BASE  512
 
-#define GPIO_RS     (GPIO_BASE + 25)  // Register Select
-#define GPIO_EN     (GPIO_BASE + 23)  // Enable
-#define GPIO_D4     (GPIO_BASE + 22)  // Data 4
-#define GPIO_D5     (GPIO_BASE + 27)  // Data 5
-#define GPIO_D6     (GPIO_BASE + 17)  // Data 6
-#define GPIO_D7     (GPIO_BASE + 4 )  // Data 7
+// Control Pins
+#define GPIO_RS     (GPIO_BASE + 25)  // Physical Pin 22
+#define GPIO_EN     (GPIO_BASE + 24)  // Physical Pin 18
+
+// Data Pins (4-bit mode)
+#define GPIO_D4     (GPIO_BASE + 23)  // Physical Pin 16
+#define GPIO_D5     (GPIO_BASE + 17)  // Physical Pin 11
+#define GPIO_D6     (GPIO_BASE + 18)  // Physical Pin 12
+#define GPIO_D7     (GPIO_BASE + 22)  // Physical Pin 15
 
 int rpi_gpio_init(void);
 void rpi_gpio_release(void);
